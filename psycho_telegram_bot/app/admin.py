85551314f247158ed30d7dcdb7d83f9621e812ea
hhux <76149411+hhux@users.noreply.github.com>
2024-10-19 20:05:25 +0700

    Update admin.py

--- psycho_telegram_bot/app/admin.py
+++ psycho_telegram_bot/app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'comment', 'is_active')
+    search_fields = ('comment',)
     readonly_fields = ('comment',)
 
 admin.site.register(User, UserAdmin)
