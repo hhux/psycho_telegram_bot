44ad78c468dcebf6cbcca1fea38793b464c2c77d
hhux <76149411+hhux@users.noreply.github.com>
2024-10-21 14:32:00 +0700

    Update views.py

--- psycho_telegram_bot/app/views.py
+++ psycho_telegram_bot/app/views.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 import logging
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
 from datetime import datetime, timedelta
