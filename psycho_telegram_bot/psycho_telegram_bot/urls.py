beedce269797525e4f653e2990af2b80fc9dca80
hhux <76149411+hhux@users.noreply.github.com>
2024-10-21 14:04:36 +0700

    Update urls.py

--- psycho_telegram_bot/psycho_telegram_bot/urls.py
+++ psycho_telegram_bot/psycho_telegram_bot/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import UserListView, UserCreateView
+from app.views import UserListView, UserCreateView, deactivate_users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', UserListView.as_view(), name='user-list'),
     path('users/create/', UserCreateView.as_view(), name='user-create'),
+    path('deactivate_users/', deactivate_users, name='deactivate-users'),
 ]
