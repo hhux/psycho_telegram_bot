6aaeed0563357276470683eb3e876ae0f4d581a4
hhux <76149411+hhux@users.noreply.github.com>
2024-10-21 14:01:25 +0700

    Delete psycho_telegram_bot/management/commands/deactivate_users.py

--- psycho_telegram_bot/management/commands/deactivate_users.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from app.deactivate_users import deactivate_users
-
-class Command(BaseCommand):
-    help = 'Deactivates users whose last payment was more than 30 days ago.'
-
-    def handle(self, *args, **kwargs):
-        deactivate_users()
