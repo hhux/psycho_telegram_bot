0a8a9b104bd045aab74c94ada67b983aa9447844
hhux <76149411+hhux@users.noreply.github.com>
2024-10-23 00:22:05 +0700

    Update shedule.sh

--- psycho_telegram_bot/shedule.sh
+++ psycho_telegram_bot/shedule.sh
@@ -1,2 +1,2 @@
-curl --location 'http://45.32.22.217:8000/deactivate_users/' \
+curl --location 'http://backend:8000/deactivate_users/' \
 --header "Authorization: Token 4bac9493762a9c397c6ffd53ff60d71400d6e99a" \
